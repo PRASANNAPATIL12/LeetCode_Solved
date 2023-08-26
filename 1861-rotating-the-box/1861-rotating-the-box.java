401e13f2fed05eab623d0eb8c499ced453cdb019
Prasannagouda S Patil <96906824+PRASANNAPATIL12@users.noreply.github.com>
2023-08-26 13:17:39 +0530

    Time: 9 ms (50.90%), Space: 67.9 MB (99.82%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -1,61 +1,33 @@
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
-        int row = box.length, col = box[0].length;
-        char[][] res = new char[col][row];
-        // move right then rotate
-        for (int i = 0; i < row; i++) {
-            for (int j = col-1; j >= 0; j--) {
-                if (box[i][j] == '#') {
-                    int curCol = j;
-                    while (curCol+1 < col && box[i][curCol+1] == '.') {
-                        curCol++;
-                    }
-                    if (curCol != j) {
-                        box[i][curCol] = '#';
-                        box[i][j] = '.';
-                    }
-                }
-            }
-        }
-        
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                res[j][i] = box[row-1-i][j];
+       box= rotate(box);
+        fall(box);
+        return box;  
+    }
+    public  char[][] rotate(char[][] box){
+        int m=box.length,n=box[0].length;
+        char[][]res=new char[n][m];
+        for(int i=0,c=m-1;i<m;c--,i++){
+            for(int j=0,r=0;j<n;r++,j++){
+                res[r][c]=box[i][j];
             }
         }
         return res;
     }
+    public  void fall(char[][] box){
+                int m=box.length,n=box[0].length;
+
+        for(int c=0;c< n;c++){
+            int l=m-1;  
+            for(int r=m-1;r>=0;r--){
+                char ch=box[r][c];
+                if(ch=='*') l=r-1;
+                else if(ch=='#'){
+                    box[r][c]='.';
+                     box[l][c]='#';
+                    l--;
+                }
+            }
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-// class Solution {
-//     public char[][] rotateTheBox(char[][] box) {
-//         for(int i=0;i<box.length;i++){
-//             int stone=0;
-//             for(int j=0;j<box[0].length;j++){
-//                 if(box[i][j]=='*'||j>=box[0].length)continue;
-//                 if(box[i][j]=='#'){
-//                     box[i][j]='.';
-//                     stone++;}
-//                 if(box[i][j]=='.')for(int k=0;k<stone;k++){
-
-//                     box[i][j-k]='#';
-//                 }
-//             }
-//         }
-// char [][] bx=new char[box[0].length][box.length];
-//          for(int i=0;i<box.length;i++)
-//          for(int j=0;j<box[0].length;j++){
-//              bx[j][i]=box[i][j];
-             
-//          }
-//          return bx;
-//     }
-// }
