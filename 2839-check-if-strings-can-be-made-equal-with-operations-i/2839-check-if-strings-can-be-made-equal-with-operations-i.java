412677e8879c1abfb7292211eee86832a8092216
Prasannagouda S Patil <96906824+PRASANNAPATIL12@users.noreply.github.com>
2023-09-02 21:50:06 +0530

    Time: 2 ms (100.00%), Space: 41.9 MB (50.00%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -5,15 +5,28 @@ class Solution {
         if(s1.equals(s2)) return true;
         
         // iterating till 1
+     
+    char arr1[] = s1.toCharArray();
+    char arr2[] = s2.toCharArray();
+    Arrays.sort(arr1);
+    Arrays.sort(arr2);
+    if(!Arrays.equals(arr1,arr2))
+        return false;
+    
+    for(int i=0;i<2;i++)
+    {
+        if(s1.charAt(i)==s2.charAt(i+2) && s1.charAt(i+2)==s2.charAt(i))
+            return true;
+    }
+    return false;
+//         for(int i=0;i<=1;i++){
+//             if(s1.charAt(i)!=s2.charAt(i) && s1.charAt(i)!=s2.charAt(i+2)) return false;
+//         }
         
-        for(int i=0;i<=1;i++){
-            if(s1.charAt(i)!=s2.charAt(i) && s1.charAt(i)!=s2.charAt(i+2)) return false;
-        }
+//         for(int i=2;i<=3;i++){
+//             if(s1.charAt(i)!=s2.charAt(i) && s1.charAt(i)!=s2.charAt(i-2)) return false;
+//         }
         
-        for(int i=2;i<=3;i++){
-            if(s1.charAt(i)!=s2.charAt(i) && s1.charAt(i)!=s2.charAt(i-2)) return false;
-        }
-        
-        return true;
+//         return true;
     }
 }
