d512c1fbae3ca92496b0bc86aec3ea2446377c37
Prasannagouda S Patil <96906824+PRASANNAPATIL12@users.noreply.github.com>
2023-09-06 23:11:20 +0530

    Time: 2 ms (35.87%), Space: 39.9 MB (98.67%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -12,13 +12,3 @@ class Solution {
     }
 }
 
-//  int sLen = s.length();
-//         int tLen = t.length();
-//         int j = 0;
-        
-//         for (int i = 0; i < tLen && j < sLen; i++) {
-//             if (s.charAt(j) == t.charAt(i)) 
-//             j++;
-//         }
-
-//         return j == sLen; 
