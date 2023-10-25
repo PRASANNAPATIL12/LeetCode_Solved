d19b20f62e0bad6cf41adf358d9596549c5bf9b2
Prasannagouda S Patil <96906824+PRASANNAPATIL12@users.noreply.github.com>
2023-10-25 21:17:19 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (90.11%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -1,13 +1,24 @@
 class Solution {
+    public int depthFirstSearch(int n, int k, int rootVal) {
+        if (n == 1) {
+            return rootVal;
+        }
+
+        int totalNodes = (int) Math.pow(2, n - 1);
+
+        // Target node will be present in the right half sub-tree of the current root node.
+        if (k > (totalNodes / 2)) {
+            int nextRootVal = (rootVal == 0) ? 1 : 0;
+            return depthFirstSearch(n - 1, k - (totalNodes / 2), nextRootVal);
+        }
+        // Otherwise, the target node is in the left sub-tree of the current root node.
+        else {
+            int nextRootVal = (rootVal == 0) ? 0 : 1;
+            return depthFirstSearch(n - 1, k, nextRootVal);
+        }
+    }
+
     public int kthGrammar(int n, int k) {
-        if(n==1) {
-            return 0;
-        }
-        int parent = kthGrammar(n-1, (k+1)/2);
-        if(k%2 == 0) {
-            return parent == 1? 0 : 1;
-        } else {
-            return parent;
-        }
+        return depthFirstSearch(n, k, 0);
     }
 }
